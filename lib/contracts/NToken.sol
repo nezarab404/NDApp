3f42180a81beb578032d242253867c82da4b9b78
nezarab404 <nezar.ab.23@gmail.com>
2022-04-01 12:43:35 -0400

    seed after migrate to ganache

--- lib/contracts/NToken.sol
+++ lib/contracts/NToken.sol
@@ -89,15 +89,4 @@ library SafeMath {
             return a / b;
         }
     }
-
-    //  function sub(
-    //     uint256 a,
-    //     uint256 b,
-    //     string memory errorMessage
-    // ) internal pure returns (uint256) {
-    //     unchecked {
-    //         require(b <= a, errorMessage);
-    //         return a - b;
-    //     }
-    // }
 }
--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -1,5 +1,10 @@
 const NToken = artifacts.require("NToken");
+const Exchange = artifacts.require("Exchange");
 
-module.exports = function (deployer) {
-  deployer.deploy(NToken);
+module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts();
+  const feeAccount = accounts[0]
+  const feePercent = 5
+  await deployer.deploy(NToken);
+  await deployer.deploy(Exchange,feeAccount,feePercent);
 };
